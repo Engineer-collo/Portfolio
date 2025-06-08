876162bebfccf926226774a714cfd0c3e2527135
Engineer-Collo <collinslikhomba@gmail.com>
2025-06-08 21:58:43 +0300

    added full height mobile navbar and 3/4 width with icons

---
+++ .history/src/components/Navbar_20250608215148.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <header className=" mt-2 bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-gray-800">        <Link to="/" className="hover:text-blue-500 text-gray-700 transition"><span className='text-blue-500'>Engineer Collins</span></Link>
+        </h1>
+
+        {/* Desktop nav */}
+        <nav className="space-x-6 hidden md:flex">
+        <Link to="/" className="hover:text-blue-500 text-gray-700 transition">Home</Link>
+          <Link to="/about" className="hover:text-blue-500 text-gray-700 transition">About</Link>
+          <Link to="/projects" className="hover:text-blue-500 text-gray-700 transition">Projects</Link>
+          <Link to="/contact" className="hover:text-blue-500 text-gray-700 transition">Contact</Link>
+        </nav>
+
+    </header>
+  );
+};
+
+export default Navbar;
